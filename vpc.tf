265324060a0a6e3d0815dc98bf259a400e16ccd2
Gee-Dee <nocturnal_monkey@yahoo.com>
2020-01-10 00:14:03 +0000

    Added VPC module take 2

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,26 @@
 module "vpc" {
-  source  = "app.terraform.io/nocturnal-creatures/vpc/aws"
-  version = "2.21.0"
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+  database_subnet_assign_ipv6_address_on_creation = false
+  elasticache_subnet_assign_ipv6_address_on_creation = false
+  enable_classiclink = false
+  enable_classiclink_dns_support = false
+  intra_subnet_assign_ipv6_address_on_creation = false
+  private_subnet_assign_ipv6_address_on_creation = false
+  public_subnet_assign_ipv6_address_on_creation = false
+  redshift_subnet_assign_ipv6_address_on_creation = false
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
